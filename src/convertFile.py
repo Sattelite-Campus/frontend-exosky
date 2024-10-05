97931cc66bcb7a283ccc335a0cf9ff73d8b2fd1a
LogicalLunatic <thompsonli@hotmail.ca>
2024-10-05 11:43:23 -0400

    Updated Star and Planet CSV, and convertFile

--- src/convertFile.py
+++ src/convertFile.py
@@ -7,24 +7,29 @@ file_path = 'PS_2024.09.28_18.08.25.csv'
 data = pd.read_csv(file_path, comment='#')
 
 # Extract the right ascension, declination, and planet name
+star_name = data['hostname']
 right_ascension = data['ra']
 declination = data['dec']
-planet_name = data['pl_name']
+distance = data['sy_dist']
+magnitude = data['sy_bmag']
 
 # Print the extracted data
+print("Star Name:", star_name)
 print("Right Ascension:", right_ascension)
 print("Declination:", declination)
-print("Planet Name:", planet_name)
+print("Distance:", distance)
+print("Luminosity:", magnitude)
 
 #create a dictionary with planet names as keys and a list of right ascension and declination as values
-planet_list = []
-for i in range(len(planet_name)):
-    temp_dict = {'name': planet_name[i], 'ra': right_ascension[i], 'dec': declination[i]}
-    planet_list.append(temp_dict)
-print(planet_list)
+star_list = []
+for i in range(len(star_name)):
+    temp_dict = {'name': star_name[i], 'ra': right_ascension[i], 'dec': declination[i], 'dist' : distance[i], 'mag' : magnitude[i]}
+    star_list.append(temp_dict)
+print(star_list)
 
+#TODO: Get Luminosity and turn luminosity into star brightness on our star map
 
 #return dictionary as json file
 import json
-with open('planet_data.json', 'w') as f:
-    json.dump(planet_list, f)
+with open('star_data.json', 'w') as f:
+    json.dump(star_list, f)
