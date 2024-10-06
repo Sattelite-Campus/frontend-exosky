19e1ce4c6acb5ed7402cf8c46c87edb4afd883c6
pekachoo <jliu6162@gmail.com>
2024-10-06 12:44:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ import {renderPlanet} from "./src/planetRendering.js";
 import {createControls} from "./src/orbitControls.js";
 import {setupButtons} from "./src/controlRendering.js";
 
+const path = window.location.pathname;
+
+const pathSegments = path.split('/');
+
+const planetName = pathSegments[1];
+
+console.log(planetName);
+
+
+// //fetch planet name
+// fetch ("", {
+//     method: "GET",
+//     headers: {
+//         "Content-Type": "application/json"
+//
+//     }
+// })
+//
+// //fetch planet data from planet name
+// fetch ("", {
+//     method: "GET",
+//     headers: {
+//         "Content-Type": "application/json"
+//
+//     }
+// })
+
 document.addEventListener("DOMContentLoaded", () => {
     setupButtons();  // Initialize the buttons
 
