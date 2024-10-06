014d1f1832dfa02191484f64a89d788c4504b733
pekachoo <jliu6162@gmail.com>
2024-10-06 07:52:35 -0400

    button + UI is good to go

--- index.js
+++ index.js
@@ -2,5 +2,36 @@ import {renderPlanet} from "./src/planetRendering.js";
 import {createControls} from "./src/orbitControls.js";
 import {exitButton} from "./src/renderUI.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const startButton = document.getElementById("start");
+    const stopButton = document.getElementById("stop");
+    const resetButton = document.getElementById("reset");
+    const toggleButton = document.getElementById("clmode");
+
+    startButton.addEventListener("click", () => {
+        console.log("Start");
+    });
+
+    stopButton.addEventListener("click", () => {
+        console.log("Stop");
+    });
+
+    resetButton.addEventListener("click", () => {
+        console.log("Reset");
+    });
+
+    toggleButton.addEventListener("click", () => {
+        console.log("Toggle");
+    });
+});
+
 renderPlanet("Data\\star_data2.json");
 // exitButton();
+
+//event listeners for UI buttons
+
+//<button id="start">Start</button>
+//     <button id="stop">Stop</button>
+//     <button id="reset">Reset</button>
+//     <button id="clmode">Toggle</button>
